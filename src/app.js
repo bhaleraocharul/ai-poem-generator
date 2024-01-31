7e7adc4da9f53202aaa50830ec7872c948c10917
charul <142137074+bhaleraocharul@users.noreply.github.com>
2024-01-31 14:59:39 +1100

    added the basic form handling and added typewriter plugin

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "hello",
+    autoStart: true,
+    cursor: null,
+    delay: 20,
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
