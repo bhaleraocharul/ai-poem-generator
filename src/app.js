1c53d42fe83ba46919b006d81ee3ba4329229f3f
charul <142137074+bhaleraocharul@users.noreply.github.com>
2024-01-31 16:29:05 +1100

    corrected the js code

--- src/app.js
+++ src/app.js
@@ -10,11 +10,11 @@ function displayPoem(response) {
 function generatePoem(event) {
   event.preventDefault();
 
-  let instructionsInput = document.querySelector("#instructions-input");
+  let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "1o630f4a9d3497230622bb9ffta16859";
   let context =
     "You are a poem expert. Make sure the poem is 4 lines long. Separate each line with <br/>. Make it rhyming";
-  let prompt = `generate a poem about ${instructionsInput}`;
+  let prompt = `generate a poem about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(displayPoem);
