2c18b686bb97594cfc85c578ed23ef51142f3f74
charul <142137074+bhaleraocharul@users.noreply.github.com>
2024-01-31 15:30:59 +1100

    added the ai api

--- src/app.js
+++ src/app.js
@@ -1,12 +1,23 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "hello",
+    strings: response.data.answer,
     autoStart: true,
     cursor: null,
     delay: 20,
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#instructions-input");
+  let apiKey = "1o630f4a9d3497230622bb9ffta16859";
+  let context =
+    "You are a poem expert. Make sure the poem is 4 lines long. Separate each line with <br/>. Make it rhyming";
+  let prompt = `generate a poem about ${instructionsInput}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
